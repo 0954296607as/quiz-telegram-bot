75aea8c232334e5a70b033a278825a0210fea852
Andrii Stashenko <0954296607as@gmail.com>
2025-10-22 06:34:57 +0000

    First start bot

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello, Quiz Telegram Bot!");
+import { BotService } from "./services/BotService";
+
+async function main() {
+    try {
+        const bot = new BotService();
+        await bot.start();
+    } catch (err) {
+        console.error("Ошибка при запуске:", err);
+    }
+}
+
+main();
